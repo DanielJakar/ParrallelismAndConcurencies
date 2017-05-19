e246e846ad10204628b74893920fbc409e583d93
DanielJakar <danieljakar@gmail.com>
2017-05-19 11:40:12 +0300

    Single thread executor

--- src/course/Daniel/Java/Main.java
+++ src/course/Daniel/Java/Main.java
@@ -2,41 +2,57 @@ package course.Daniel.Java;
 
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 public class Main {
 
-    private static final Object LOCK = new Object() ;
+//    private static final Object LOCK = new Object() ;
 
     public static void main(String[] args) {
 
-        SheepHerd herd = new SheepHerd();
+        ExecutorService service = Executors.newSingleThreadExecutor();
 
-        for (int i = 0; i < 10; i++) {
-            new Thread(() -> herd.addSheepAndCount(), String.valueOf(i)).start();
+        service.execute(() -> System.out.println("Running the task....."));
+        System.out.println("main");
+        service.execute(() -> System.out.println("Running the task....."));
+        System.out.println("main");
+        service.execute(() -> System.out.println("Running the task....."));
+        System.out.println("main");
+        service.execute(() -> System.out.println("Running the task....."));
+        System.out.println("main");
 
-        }
-
+        service.shutdown();
     }
 
-    public static void doSomeWork () throws InterruptedException {
-        System.out.println(Thread.currentThread().getName());
-        Thread.sleep(1000);
-
-        //Thread safe.
-        synchronized (LOCK){
-            //atomic unit of execution
-            //increment your counter safely
-            System.out.println(Thread.currentThread().getName() + " Is Alone Here");
-
-        }
-
-        Thread.sleep(1000);
-        System.out.println(Thread.currentThread().getName());
-
-    }
-
-
+//        SheepHerd herd = new SheepHerd();
+//
+//        for (int i = 0; i < 10; i++) {
+//            new Thread(() -> herd.addSheepAndCount(), String.valueOf(i)).start();
+//
+//        }
+//
+//    }
+//
+//    public static void doSomeWork () throws InterruptedException {
+//        System.out.println(Thread.currentThread().getName());
+//        Thread.sleep(1000);
+//
+//        //Thread safe.
+//        synchronized (LOCK){
+//            //atomic unit of execution
+//            //increment your counter safely
+//            System.out.println(Thread.currentThread().getName() + " Is Alone Here");
+//
+//        }
+//
+//        Thread.sleep(1000);
+//        System.out.println(Thread.currentThread().getName());
+//
+//    }
+//
+//
 
 
 //        // favour encapsulation over inheritance
